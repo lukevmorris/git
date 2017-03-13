20d59ab335211ee7c51716d62a5fb46dbe404773
Brandon Williams <bmwill@google.com>
2017-03-13 14:43:36 -0700

    submodule: decouple url and submodule existence
    Currently the submodule.<name>.url config option is used to determine
    if a given submodule exists and is interesting to the user.  This
    however doesn't work very well because the URL is a config option for
    the scope of a repository, whereas the existence of a submodule is an
    option scoped to the working tree.
    
    In a future with worktree support for submodules, there will be multiple
    working trees, each of which may only need a subset of the submodules
    checked out.  The URL (which is where the submodule repository can be
    obtained) should not differ between different working trees.
    
    It may also be convenient for users to more easily specify groups of
    submodules they are interested in as apposed to running "git submodule
    init <path>" on each submodule they want checked out in their working
    tree.
    
    To this end two config options are introduced, submodule.active and
    submodule.<name>.active.  The submodule.active config holds a pathspec
    that specifies which submodules should exist in the working tree.  The
    submodule.<name>.active config is a boolean flag used to indicate if
    that particular submodule should exist in the working tree.
    
    Given these multiple ways to check for a submodule's existence the more
    fine-grained submodule.<name>.active option has the highest order of
    precedence followed by the pathspec check against submodule.active. To
    ensure backwards compatibility, if neither of these options are set git
    falls back to checking the submodule.<name>.url option to determine a
    submodule's existence.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7413-submodule-is-active.sh
+++ t/t7413-submodule-is-active.sh
@@ -28,4 +28,59 @@ test_expect_success 'is-active works with urls' '
 	git -C super submodule--helper is-active sub1
 '
 
+test_expect_success 'is-active works with submodule.<name>.active config' '
+	git -C super config --bool submodule.sub1.active "false" &&
+	test_must_fail git -C super submodule--helper is-active sub1 &&
+
+	git -C super config --bool submodule.sub1.active "true" &&
+	git -C super config --unset submodule.sub1.URL &&
+	git -C super submodule--helper is-active sub1 &&
+
+	git -C super config submodule.sub1.URL ../sub &&
+	git -C super config --unset submodule.sub1.active
+'
+
+test_expect_success 'is-active works with basic submodule.active config' '
+	git -C super config --add submodule.active "." &&
+	git -C super config --unset submodule.sub1.URL &&
+
+	git -C super submodule--helper is-active sub1 &&
+	git -C super submodule--helper is-active sub2 &&
+
+	git -C super config submodule.sub1.URL ../sub &&
+	git -C super config --unset-all submodule.active
+'
+
+test_expect_success 'is-active correctly works with paths that are not submodules' '
+	test_must_fail git -C super submodule--helper is-active not-a-submodule &&
+
+	git -C super config --add submodule.active "." &&
+	test_must_fail git -C super submodule--helper is-active not-a-submodule &&
+
+	git -C super config --unset-all submodule.active
+'
+
+test_expect_success 'is-active works with exclusions in submodule.active config' '
+	git -C super config --add submodule.active "." &&
+	git -C super config --add submodule.active ":(exclude)sub1" &&
+
+	test_must_fail git -C super submodule--helper is-active sub1 &&
+	git -C super submodule--helper is-active sub2 &&
+
+	git -C super config --unset-all submodule.active
+'
+
+test_expect_success 'is-active with submodule.active and submodule.<name>.active' '
+	git -C super config --add submodule.active "sub1" &&
+	git -C super config --bool submodule.sub1.active "false" &&
+	git -C super config --bool submodule.sub2.active "true" &&
+
+	test_must_fail git -C super submodule--helper is-active sub1 &&
+	git -C super submodule--helper is-active sub2 &&
+
+	git -C super config --unset-all submodule.active &&
+	git -C super config --unset submodule.sub1.active &&
+	git -C super config --unset submodule.sub2.active
+'
+
 test_done
